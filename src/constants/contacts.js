997210d749dc5b276cf4f4553f3859e8c8d39194
Myroslava <gordinskamiroslava@gmail.com>
2024-06-04 11:46:55 +0200

    add

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,3 +1,7 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export const PATH_DB = path.join('src', 'db', 'db.json');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const PATH_DB = path.join(__dirname, '..', 'db', 'db.json');
--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -2,18 +2,20 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
 
 export const thanos = async () => {
-    
-    const read = await fs.readFile( PATH_DB, 'utf-8');
+    // Читання даних з файлу
+    const read = await fs.readFile(PATH_DB, 'utf-8');
     const myArray = JSON.parse(read);
 
-  
+    // Відбір контактів
     const thanos = myArray.filter(() => Math.random() >= 0.5);
 
-   
+    // Підготовка даних для запису
     const update = JSON.stringify(thanos, null, 2);
 
-    await fs.writeFile('filtered_data.json', update + '\n');
+    // Запис у файл db.json
+    await fs.writeFile(PATH_DB, update + '\n');
 
+    // Вивід результату у консоль
     console.log(thanos);
 };
 
